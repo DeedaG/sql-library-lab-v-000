836a2224fa1df33cbcd94895240ee2054ada56f3
Christia T <christiatidwell@bellsouth.net>
2018-08-12 01:33:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title, characters.species FROM series
   INNER JOIN characters ON series.id = characters.series_id
-  WHERE characters.species = human;"
+  GROUP BY characters.species = human ORDER BY series.title DESC ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
