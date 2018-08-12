6449d9d4a9cc19eedf51d853d9be0aae2632f186
Christia T <christiatidwell@bellsouth.net>
2018-08-12 01:06:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,19 +1,26 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY length(motto) DESC
+    LIMIT 1;"
 end
 
-
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) AS most_profilic FROM characters
+  GROUP BY species ORDER BY most_profilic DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT series.subgenre_id, series.author_id, authors.name FROM series
+  INNER JOIN series ON series.author_id = authors.author_id;"
 end
+
+SELECT column_name(s)
+FROM first_table
+INNER JOIN second_table
+ON first_table.column_name = second_table.column_name;
 
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
