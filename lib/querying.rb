ab3ef5af666a94c6dc3aa4a261220b0a13281ad3
Christia T <christiatidwell@bellsouth.net>
2018-08-12 01:14:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,8 +13,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT series.subgenre_id, authors.name FROM series
-  INNER JOIN authors ON series.author_id = authors.id;"
+  "SELECT authors.name, subgenre.name FROM series
+  INNER JOIN authors ON series.author_id = authors.id 
+  INNER JOIN series ON subgenres.;"
 end
 
 def select_series_title_with_most_human_characters
